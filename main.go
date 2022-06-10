9d02c9bd03e4d07a7c8a33ef9e31b1f7d821b5db
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2022-06-10 16:34:08 -0700

    Append line timestamp to records properly & use separate variable for it
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

--- main.go
+++ main.go
@@ -221,10 +221,11 @@ func main() {
 
 		// Since we're not getting a timestamp from the serial device,
 		// we create one here & prepend it to `records`.
-		ts := time.Now().UnixNano()
+		line_ts := float64(time.Now().UnixNano())
 		// ...and divide by 1e9 to match up with my timestamp code up
 		// ahead.  My, what a bletcherous hack.
-		ts /= 1e9
+		line_ts /= 1e9
+		records = append([]string{fmt.Sprintf("%f", line_ts)}, records...)
 
 		// Create a point and add to batch
 		tags := map[string]string{}
